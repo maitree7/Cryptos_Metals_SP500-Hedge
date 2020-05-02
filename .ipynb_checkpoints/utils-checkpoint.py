06ed09d392e77bb4403ebd65306e8fc7a430b0b6
Maitree <maitree7@gmail.com>
2020-05-01 23:06:38 -0400

    Cryptocurrency Data

---
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+import numpy as np
+
+
+def get_daily_data(api_key, from_symbol, to_symbol='USD'):
+    resp = requests.get('https://www.alphavantage.co/query', params={
+        'function': 'DIGITAL_CURRENCY_DAILY',
+        'symbol': from_symbol,
+        'market': to_symbol,
+        'apikey': api_key
+    })
+
+    resp.raise_for_status()
+    doc = resp.json()
+    if 'Error Message' in doc:
+        raise ValueError(doc['Error Message'])
+    
+    df = pd.DataFrame.from_dict(doc['Time Series (Digital Currency Daily)'], orient='index', dtype=np.float)
+    df.drop(columns=[c for c in df.columns.values if 'b.' in c], inplace=True)
+    df.reset_index(inplace=True)
+    df['index'] = pd.to_datetime(df['index'])
+    df.set_index('index', inplace=True)
+    
+    return df
+    
+    
